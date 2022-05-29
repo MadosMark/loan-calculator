658403bf7dd1d1db9cc813e25c1edd84e9e94188
Mark Mados <mark.mados@kolon.studio>
2022-05-29 21:14:04 +0200

    Working on project

--- loan-calculator/src/App.js
+++ loan-calculator/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Slider } from "@mui/material";
 
 function App() {
+  const [value, setValue] = React.useState(20000);
+  const [valuee, setValuee] = React.useState(2);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
+  const handleChangee = (e, newValuee) => {
+    setValuee(newValuee);
+  };
+  const amount = value;
+  const years = valuee;
+  const interest = 0.00825;
+  const months = years * 12;
+  const x = Math.pow(1 + interest, months);
+  const monthly = (amount * x * interest) / (x - 1);
+  let monthlyPayment = Math.ceil(monthly);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <div className="wrapper">
+        <p>Lånekalkyl</p>
+        <div className="slider">
+          <p className="choose">Välj Lånebelopp</p>
+          <Slider
+            defaultValue={20000}
+            aria-label="Default"
+            step={10000}
+            min={20000}
+            max={200000}
+            value={value}
+            onChange={handleChange}
+            color="warning"
+          />
+          <p className="value">{value} kr</p>
+          <p className="choose">Välj Återbetalningstid</p>
+          <Slider
+            className="slide"
+            defaultValue={2}
+            aria-label="Default"
+            step={1}
+            min={2}
+            max={10}
+            value={valuee}
+            onChange={handleChangee}
+            color="warning"
+          />
+          <p className="value">{valuee} år</p>
+        </div>
+
+        <p>Månadskostnad:</p>
+        {valuee ? (
+          <p>{monthlyPayment}kr/månad</p>
+        ) : (
+          <p className="calc">Uträkning kommer visas här</p>
+        )}
+      </div>
+      <button className="button">
+        <a href={`loan-application/?amount=${value}&months=${valuee}`}>
+          Till Ansökan
         </a>
-      </header>
+      </button>
     </div>
   );
 }
